171a4e0c35eb3cf5c4b828b45a26117bdba75a58
Eugene Popov <Eugene_Popov@cable.comcast.com>
2019-09-04 12:14:34 +0300

    Implement Truncate module for task #1
    This module allows to truncate string attributes

--- task_1/truncate.rb
+++ task_1/truncate.rb
@@ -1,2 +1,22 @@
 module Truncate
+  # Memoize old attribute getter method because define method will override it when truncate
+  # will be called
+  def memoize(attr_name)
+    alias_method "memoized_#{attr_name}", attr_name
+  end
+
+  def truncate(attr_name, length:)
+    memoize(attr_name)
+    define_method(attr_name.to_sym) do
+      original_value = self.public_send("memoized_#{attr_name}")
+      raise AttributeError.new(attr_name) unless original_value.is_a?(String)
+      original_value[0, length.to_i]
+    end
+  end
+
+  class AttributeError < StandardError
+    def initialize(attr_name)
+      super("#{attr_name} is not a string. Truncate only supports string attributes")
+    end
+  end
 end

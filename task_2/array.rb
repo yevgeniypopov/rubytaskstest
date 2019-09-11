cfe990d21e7b71aa6b2dc5482c0bcb0a91b74aca
Eugene Popov <Eugene_Popov@cable.comcast.com>
2019-09-11 12:21:58 +0300

    Implement Array rebuild! via recursive method

--- task_2/array.rb
+++ task_2/array.rb
@@ -1,7 +1,19 @@
 module CoreExtensions
   module Array
-    def rebuild!
-      puts 'TEST'
+    def rebuild!(&block)
+      # @nestness shows how nested is current array
+      # we use it to update array elements that are not nested in other arrays 
+      @nestness = 0
+
+      self.each_with_index do |item, index|
+        if item.is_a?(Array)
+          @nestness = @nestness + 1
+          item.rebuild!(&block)
+        else
+          length = @nestness.zero? ? self.length : 1
+          self[index] = yield(item, length)
+        end
+      end
     end
   end
 end

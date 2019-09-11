b930687aa4c0fbb825fb6f87aab45589c0da0897
Eugene Popov <Eugene_Popov@cable.comcast.com>
2019-09-11 12:51:16 +0300

    Add more comments

--- task_2/array.rb
+++ task_2/array.rb
@@ -2,7 +2,7 @@ module CoreExtensions
   module Array
     def rebuild!(&block)
       # @nestness shows how nested is current array
-      # we use it to update array elements that are not nested in other arrays 
+      # we use it to update array elements that are not nested in other arrays
       @nestness = 0
 
       self.each_with_index do |item, index|
@@ -18,4 +18,7 @@ module CoreExtensions
   end
 end
 
+# I'd prefer to use ruby refinements for this monkey patch, but according to task rules all work
+# should be done in array.rb file
+# https://docs.ruby-lang.org/en/2.4.0/syntax/refinements_rdoc.html
 Array.include CoreExtensions::Array
